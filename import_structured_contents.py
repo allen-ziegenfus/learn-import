8419d0d83b640f28a9724a66e8cff49b1c330281
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2022-08-29 22:16:38 -0500

    ISSITES-5639 Fix syntax errors

--- import_structured_contents.py
+++ import_structured_contents.py
@@ -119,11 +119,6 @@ def get_structured_content_request_body(sphinx_article: dict):
                 "contentFieldValue_i18n": contentFieldValues["navtoc"],
                 "name": "Navigation",
             },
-            {
-                "contentFieldValue": {"data": ""},
-                "contentFieldValue_i18n": {DEFAULT_LANGUAGE_ID: {"data": ""}},
-                "name": "githubEditLink",
-            },
         ],
         "contentStructureId": config["ARTICLE_STRUCTURE_ID"],
         # friendlyUrlPaths are all lower case (otherwise Liferay will modify it)
@@ -139,7 +134,7 @@ def get_structured_content_request_body(sphinx_article: dict):
     for liferay_language_id in available_languages:
         contentFieldValues["sha_256sum"][liferay_language_id] = {"data": sha_256sum}
 
-    translatedArticle["contentFields"].append(
+    structured_content_request_body["contentFields"].append(
         {
             "contentFieldValue": {"data": ""},
             "contentFieldValue_i18n": contentFieldValues["sha_256sum"],
